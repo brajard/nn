b924061d8e61133815fd662e467f197dd299a6c7
brajard <julien.brajard@inria.fr>
2016-12-13 11:57:59 +0100

    add save mat and fig in test models

--- lasagne/test_model.py
+++ lasagne/test_model.py
@@ -7,11 +7,14 @@ from datatools import plot_sequence,plot_scatter
 from keras.models import model_from_json
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.io import savemat
 
-outdir = '../data/nn_rec2'
+outdir = '../data/nn_rec4'
 modelname = 'rnn.json'
 weights = 'weights.h5'
 data =  'data.npz'
+tosave = True
+tosavemat = True
 
 plt.close("all")
 
@@ -28,7 +31,7 @@ ypred = model.predict(Xapp).squeeze()
 
 plot_sequence(yapp.reshape([-1,7,7]),ypred.reshape([-1,7,7]),start=start,length=7)
 
-ypers = Xapp.squeeze()[:,-1,:].reshape(Xapp.shape[0],-1)
+ypers = Xapp[:,-1,0].reshape(Xapp.shape[0],-1)
 plot_sequence(yapp.reshape([-1,7,7]),ypers.reshape([-1,7,7]),start=start,length=7)
 
 num_pix = 24
@@ -41,3 +44,15 @@ plot_scatter(yapp_c,ypred_c)
 
 print("persistence performance:")
 plot_scatter(yapp_c,ypers_c)
+
+figtitle=['Seq_nn','Seq_pers','scatter_nn','scatter_pers']
+
+for i in range(1,5):
+    plt.figure(i)
+    plt.title(figtitle[i-1])
+    if tosave:
+        plt.savefig(os.path.join(outdir,figtitle[i-1]+'.png'))
+
+dmat={'Xapp':Xapp,'yapp':yapp,'ypred':ypred}
+if tosavemat:
+    savemat(os.path.join(outdir,'matfile.mat'),dmat)
