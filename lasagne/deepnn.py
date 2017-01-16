19409a112a1e0910e78f46f385eacf52f93e9c0a
brajard <julien.brajard@inria.fr>
2017-01-16 17:52:45 +0100

    handle other types of networks (lstm, dense, CNN)

--- lasagne/deepnn.py
+++ lasagne/deepnn.py
@@ -1,12 +1,16 @@
 """
 This is a module to make a equivalent to scikit-learn estimator
 with a deep neural network implemented in Keras"""
+import datatools
+from importlib import reload
+reload(datatools)
+
 import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.utils.validation import check_X_y, check_array
 from operator import mul
 from functools import reduce
-from datatools import define_model_all
+from datatools import define_model_all,  define_model_lstm, define_model_Dense, define_model_Conv
 
 #Some remarks
 #all parameters should be named the same way in __init__method and for attributes
@@ -51,21 +55,22 @@ class kerasnn (BaseEstimator, RegressorMixin):
         self.init_=init_
         self.lr_=lr_
         self.network_type_=network_type_
-        self.paramset_ = {'shapef_','n_feat_in_','filter_size_in_','n_feat_out_','filter_size_out_','nhid1_','nhid2_','init_',
-                          'pool_size_','batch_size_','nb_epoch_','validation_split_','lr_','network_type_'}
+        self.paramset_ = {'shapef_','n_feat_in_','filter_size_in_','n_feat_out_','filter_size_out_','nhid1_',
+                          'nhid2_','init_','pool_size_',
+                          'batch_size_','nb_epoch_','validation_split_','lr_','network_type_'}
 
     def reshape(self,X):
-        if self.network_type_ == 'all' or self.network_type_ == 'lstm'
+        if self.network_type_ == 'all' or self.network_type_ == 'lstm':
             nfeature = reduce(mul,self.shapef_)
             assert(nfeature==X.shape[1]),'wrong size of X'
             self.shape_ = tuple([X.shape[0]])+tuple(self.shapef_) 
             self.XX_ = X.reshape(self.shape_)
-        elif self.network_type_ == 'dense' or self.network_type_ == 'conv'
+        elif self.network_type_ == 'dense' or self.network_type_ == 'conv':
             nfeature = reduce(mul,self.shapef_)
             assert(nfeature==X.shape[1]),'wrong size of X'
-            self.shape = tuple([X.shape[0]])+tuple(self.shapef_[0]*self.shapef[1])+tuple(self.shapef_[2:])           
+            self.shape_ = tuple([X.shape[0]])+tuple([self.shapef_[0]*self.shapef_[1]])+tuple(self.shapef_[2:])
             self.XX_ = X.reshape(self.shape_)
-        else
+        else:
             raise ValueError('not a valid model type')
         
         
@@ -74,33 +79,39 @@ class kerasnn (BaseEstimator, RegressorMixin):
         X, y = check_X_y(X,y,multi_output=True)
         self.reshape(X) #compute self._XX
         self.y_ = y
-        if self.network_type_ == 'all'
-            self.nn_ = define_model_all(shape = self.shape_,\
-                                    n_feat=self.n_feat_, filter_size= self.filter_size_,\
-                                    nhid1=self.nhid1_, nhid2 = self.nhid2_,\
-                                    pool_size = self.pool_size_,lr=self.lr_)
-        elif self.network_type_ == 'lstm'
-            self.nn_ = define_model_lstm(shape = self.shape_,\
-                                    n_feat=self.n_feat_, filter_size= self.filter_size_,\
-                                    nhid1=self.nhid1_, nhid2 = self.nhid2_,\
-                                    pool_size = self.pool_size_,lr=self.lr_)
-        elif self.network_type_ == 'dense'
-            self.nn_ = define_model_Dense(shape = self.shape_,\
-                                    n_feat=self.n_feat_, filter_size= self.filter_size_,\
-                                    nhid1=self.nhid1_, nhid2 = self.nhid2_,\
-                                    pool_size = self.pool_size_,lr=self.lr_)
-        elif self.network_type_ == 'conv'
-            self.nn_ = define_model_Conv(shape = self.shape_,\
-                                    n_feat=self.n_feat_, filter_size= self.filter_size_,\
-                                    nhid1=self.nhid1_, nhid2 = self.nhid2_,\
-                                    pool_size = self.pool_size_,lr=self.lr_)
-        else
+        if self.network_type_ == 'all':
+            self.nn_ = define_model_all(shape = self.shape_,
+                                        n_feat_in=self.n_feat_in_, n_feat_out=self.n_feat_out_,
+                                        filter_size_in= self.filter_size_in_,
+                                        filter_size_out= self.filter_size_out_,
+                                        nhid1=self.nhid1_, nhid2 = self.nhid2_,
+                                        pool_size = self.pool_size_,lr=self.lr_)
+        elif self.network_type_ == 'lstm':
+            print("!!! lstm !!!")
+
+            self.nn_ = define_model_lstm(shape = self.shape_,
+                                    nhid1=self.nhid1_, nhid2 = self.nhid2_,
+                                    lr=self.lr_)
+        elif self.network_type_ == 'dense':
+            self.nn_ = define_model_Dense(shape = self.shape_,
+                                    nhid1=self.nhid1_, nhid2 = self.nhid2_,
+                                    lr=self.lr_)
+        elif self.network_type_ == 'conv':
+            print("!!! conv !!!")
+                        
+            self.nn_ = define_model_Conv(shape = self.shape_,
+                                         n_feat_in=self.n_feat_in_, n_feat_out=self.n_feat_out_,
+                                         filter_size_in= self.filter_size_in_,
+                                         filter_size_out= self.filter_size_out_,
+                                         nhid=self.nhid1_,
+                                         pool_size = self.pool_size_,lr=self.lr_)
+        else:
             raise ValueError('not a valid model type')
 
   
         self.history_ = self.nn_.fit(self.XX_,self.y_,batch_size=self.batch_size_,\
                                   nb_epoch=self.nb_epoch_,\
-                                  validation_split=self.validation_split_,verbose=0)
+                                  validation_split=self.validation_split_,verbose=1)
     
         return self
 
@@ -115,7 +126,8 @@ class kerasnn (BaseEstimator, RegressorMixin):
         return {name:getattr(self,name) for name in self.paramset_}
 
     def set_params(self,**parameters):
-        #TODO Test if parameters are correct ?
         for parameter, value in parameters.items():
+            if not parameter in self.paramset_:
+                raise ValueError ("Parameter '"+parameter+"' is not in the admissible list")
             setattr(self,parameter,value)
         return self
