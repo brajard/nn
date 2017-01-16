f281815f18d4713506e3f9cf9783c98a534f4549
brajard <julien.brajard@inria.fr>
2017-01-16 16:11:33 +0100

    more recent config of the best nn

--- lasagne/script_train.py
+++ lasagne/script_train.py
@@ -9,14 +9,14 @@ reload(datatools)
 from datatools import prepare_data, define_model_all,make_train
 import theano
 
-theano.config.optimizer = 'None'
+#theano.config.optimizer = 'None'
 
 import numpy as np
 
 import matplotlib.pyplot as plt
 
 plt.close("all")
-outdir = '../data/nn_rec1'
+outdir = '../data/nn_rec7'
 
 datadir = '/net/argos/data/parvati/aaclod/home2/aaclod/MAREES/python_nn/nn'
 fname = 'MATRICE_01_2017.mat'
@@ -24,13 +24,13 @@ geofile = 'USABLE_PointbyPoint_01_2017.mat'
 data,scaler = prepare_data(datadir=datadir,fname=fname,geofile=geofile,lognorm=True,epsi=0.0001,smNum = [44,55,66,77,88,99,110])
 
 
-n_feat = 5
+n_feat = 7
 filter_size=3
 nhid1 = 12
-nhid2 = 12
+nhid2 = 10
 pool_size = (2,2)
 
-model = define_model_all(data.Xapp.shape,n_feat=n_feat,filter_size=filter_size,nhid1=nhid1,nhid2=nhid2,pool_size=pool_size)
+model = define_model_all(shape=data.Xapp.shape,n_feat=n_feat,filter_size=filter_size,nhid1=nhid1,nhid2=nhid2,pool_size=pool_size,lr=0.01)
 
 
-make_train(data,model,outdir,nb_epoch=2)
+make_train(data,model,outdir,nb_epoch=100)
