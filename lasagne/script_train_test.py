1de126463e125fc6856acb2d6ffd7a08874ea5d8
brajard <julien.brajard@upmc.fr>
2017-02-15 10:47:49 +0100

    small modif

--- lasagne/script_train_test.py
+++ lasagne/script_train_test.py
@@ -7,6 +7,7 @@ import os
 from deepnn import kerasnn
 from datatools import prepare_data, make_train
 import theano
+import time
 
 #theano.config.optimizer = 'None'
 
@@ -20,8 +21,11 @@ outdir = '../data/nn_test'
 
 data,scaler = prepare_data(lognorm=True,epsi=0.0001,smNum = [55,77])
 
-params = {'n_feat_in_': 5, 'network_type_': 'all', 'n_feat_out_': 7, 'nhid2_': 10, 'nhid1_': 12}
-
-model = kerasnn(shapef_=data.Xval.shape[1:],nb_epoch_=20)
+params = {'n_feat_in_': 5, 'network_type_': 'all', 'n_feat_out_': 7, 'nhid2_': 10, 'nhid1_': 12, 'batch_size_':128}
+model = kerasnn(shapef_=data.Xval.shape[1:],nb_epoch_=5)
 model.set_params(**params)
+print('starting...')
+start = time.time()
 make_train(data,model,outdir)
+end = time.time()
+print('elapsed time: ',end-start)
